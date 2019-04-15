7399a8d3f1a7cd74ec7be2f15038c9e8d66b0b10
apple <apple@oit-007.local>
2019-04-15 21:38:30 +0900

    基本設定

---
+++ DYZB/DYZB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DYZB
+//
+//  Created by apple on 2019/04/13.
+//  Copyright © 2019年 com.btx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
